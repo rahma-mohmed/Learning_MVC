f145e4a493ec79639909cfeaec5823c3bdd66c5b
Rahma-Mohmed <99695242+rahma-mohmed@users.noreply.github.com>
2024-05-09 22:19:08 +0300

    Delete Fayroz directory

--- Fayroz/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fayroz.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
